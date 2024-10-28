f7e42d76f3b7e489d110647aeac38741eedd3c51
Oleh-Kazydub AP-13 <165385708+Oleh-kazydub@users.noreply.github.com>
2024-10-29 00:18:41 +0300

    Update product.cpp

--- Labs_Kazydub/laba_6/product.cpp
+++ Labs_Kazydub/laba_6/product.cpp
@@ -1,4 +1,5 @@
 #include "product.h"
+
 // Конструктор за замовчуванням
 Product::Product() {
     for (int i = 0; i < SIZE; ++i) {
@@ -8,20 +9,26 @@ Product::Product() {
         products[i].quantity = 0;
     }
 }
+
 // Деструктор
-Product::~Product() {
-}
+Product::~Product() {}
+
 // Введення даних про товари
 void Product::input() {
     for (int i = 0; i < SIZE; ++i) {
         std::cout << "Enter ID for product " << i + 1 << ": ";
         std::cin >> products[i].id;
+
         std::cout << "Enter name for product " << i + 1 << ": ";
-        std::cin >> products[i].name;
+        std::cin.ignore(); // Clear the newline character from the input buffer
+        std::getline(std::cin, products[i].name); // Use getline for name input
+
         std::cout << "Enter price for product " << i + 1 << ": ";
         std::cin >> products[i].price;
+
         std::cout << "Enter quantity for product " << i + 1 << ": ";
         std::cin >> products[i].quantity;
+
         // Перевірка даних
         setId(products[i].id);
         setName(products[i].name);
@@ -29,16 +36,18 @@ void Product::input() {
         setQuantity(products[i].quantity);
     }
 }
+
 // Виведення даних про товари
 void Product::output() const {
     for (const auto& product : products) {
-        std::cout << "ID: " << product.id
-                  << ", Name: " << product.name
-                  << ", Price: " << product.price
-                  << ", Quantity: " << product.quantity
+        std::cout << "ID: " << product.id 
+                  << ", Name: " << product.name 
+                  << ", Price: " << product.price 
+                  << ", Quantity: " << product.quantity 
                   << std::endl;
     }
 }
+
 // Перевірка і встановлення ціни
 void Product::setPrice(double price) {
     if (price >= 0.0) {
@@ -47,6 +56,7 @@ void Product::setPrice(double price) {
         std::cout << "Invalid price!" << std::endl;
     }
 }
+
 // Перевірка і встановлення імені
 void Product::setName(const std::string& name) {
     if (!name.empty()) {
@@ -55,6 +65,7 @@ void Product::setName(const std::string& name) {
         std::cout << "Invalid name!" << std::endl;
     }
 }
+
 // Перевірка і встановлення ID
 void Product::setId(int id) {
     if (id > 0) {
@@ -63,6 +74,7 @@ void Product::setId(int id) {
         std::cout << "Invalid ID!" << std::endl;
     }
 }
+
 // Перевірка і встановлення кількості
 void Product::setQuantity(int quantity) {
     if (quantity >= 0) {
@@ -71,3 +83,4 @@ void Product::setQuantity(int quantity) {
         std::cout << "Invalid quantity!" << std::endl;
     }
 }
+
