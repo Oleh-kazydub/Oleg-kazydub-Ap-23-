d58d1da7b0f2105dbfe108a36ddff7b80a595966
Oleh-Kazydub AP-13 <165385708+Oleh-kazydub@users.noreply.github.com>
2024-11-04 22:55:28 +0300

    Create AircraftCarrier.cpp

---
+++ Labs_Kazydub/laba_9/AircraftCarrier.cpp
@@ -0,0 +1,16 @@
+#include "AircraftCarrier.h"
+#include <iostream>
+
+AircraftCarrier::AircraftCarrier(const std::string& name, double displacement, int numberOfAircraft)
+    : Ship(name, displacement), numberOfAircraft(numberOfAircraft) {}
+
+AircraftCarrier::~AircraftCarrier() {}
+
+void AircraftCarrier::display() const {
+    Ship::display();
+    std::cout << "Number of Aircraft: " << numberOfAircraft << std::endl;
+}
+
+int AircraftCarrier::getNumberOfAircraft() const {
+    return numberOfAircraft;
+}
