7fb2c9e0344667cc86316760264e8878fdbd7638
Oleh-Kazydub AP-13 <165385708+Oleh-kazydub@users.noreply.github.com>
2024-11-23 20:04:01 +0300

    Update DataHandler.cpp

--- Labs_Kazydub/laba_9/DataHandler.cpp
+++ Labs_Kazydub/laba_9/DataHandler.cpp
@@ -1,15 +1,11 @@
 #include "DataHandler.h"
 #include <iostream>
 
-void DataHandler::display(const Ship& ship) {
-    ship.display();  // Викликається метод display() для кожного корабля
-}
-
 void DataHandler::calculateTotalDisplacement(const std::vector<Ship*>& ships) {
     double totalDisplacement = 0;
     for (const auto& ship : ships) {
         totalDisplacement += ship->getDisplacement();
     }
-    std::cout << "\nЗагальна водотоннажність всіх кораблів: " << totalDisplacement << " тон" << std::endl;
+    std::cout << "Загальна водотоннажність всіх кораблів: " << totalDisplacement << " тонн\n";
 }
 
