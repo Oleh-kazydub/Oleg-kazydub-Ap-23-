d700900e8012740fce8e0c0f4fa9791a84c56a05
Oleh-Kazydub AP-13 <165385708+Oleh-kazydub@users.noreply.github.com>
2024-11-23 20:03:06 +0300

    Update Ship.cpp

--- Labs_Kazydub/laba_9/Ship.cpp
+++ Labs_Kazydub/laba_9/Ship.cpp
@@ -1,13 +1,10 @@
 #include "Ship.h"
 #include <iostream>
 
-Ship::Ship(const std::string& name, double displacement) : name(name), displacement(displacement) {}
+Ship::Ship(const std::string& name, double displacement)
+    : name(name), displacement(displacement) {}
 
 Ship::~Ship() {}
-
-void Ship::display() const {
-    std::cout << "Ship Name: " << name << ", Displacement: " << displacement << " tons" << std::endl;
-}
 
 double Ship::getDisplacement() const {
     return displacement;
