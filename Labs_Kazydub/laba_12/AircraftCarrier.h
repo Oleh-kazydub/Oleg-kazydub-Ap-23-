630004487656763bbeac51c68e6b31772886fe03
Oleh-Kazydub AP-13 <165385708+Oleh-kazydub@users.noreply.github.com>
2024-11-26 12:49:08 +0200

    Update AircraftCarrier.h

--- Labs_Kazydub/laba_12/AircraftCarrier.h
+++ Labs_Kazydub/laba_12/AircraftCarrier.h
@@ -5,11 +5,29 @@
 
 class AircraftCarrier : public Ship {
 public:
-    AircraftCarrier(std::string n = "", double l = 0, double t = 0, int c = 0);
-    void displayInfo() const override;
-    void input() override;
-    void saveToFile(std::ofstream& outFile) override;
-    void loadFromFile(std::ifstream& inFile) override;
+    AircraftCarrier(std::string n = "", double l = 0, double t = 0, int c = 0)
+        : Ship(n, l, t, c) {}
+
+    void displayInfo() const override {
+        std::cout << "Авіаносець: " << name << std::endl;
+        Ship::displayInfo();
+    }
+
+    void input() override {
+        std::cout << "Введіть інформацію для Авіаносця:\n";
+        Ship::input();
+    }
+
+    void saveToFile(std::ofstream& outFile) override {
+        outFile << "Авіаносець\n";
+        Ship::saveToFile(outFile);
+    }
+
+    void loadFromFile(std::ifstream& inFile) override {
+        std::string temp;
+        std::getline(inFile, temp); // Читання "Авіаносець"
+        Ship::loadFromFile(inFile);
+    }
 };
 
 #endif // AIRCRAFTCARRIER_H
