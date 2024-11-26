44d320aae8e083d4c318a2f09243787aff7aabf7
Oleh-Kazydub AP-13 <165385708+Oleh-kazydub@users.noreply.github.com>
2024-11-26 12:48:50 +0200

    Update Cruiser.h

--- Labs_Kazydub/laba_12/Cruiser.h
+++ Labs_Kazydub/laba_12/Cruiser.h
@@ -5,11 +5,30 @@
 
 class Cruiser : public Ship {
 public:
-    Cruiser(std::string n = "", double l = 0, double t = 0, int c = 0);
-    void displayInfo() const override;
-    void input() override;
-    void saveToFile(std::ofstream& outFile) override;
-    void loadFromFile(std::ifstream& inFile) override;
+    Cruiser(std::string n = "", double l = 0, double t = 0, int c = 0)
+        : Ship(n, l, t, c) {}
+
+    void displayInfo() const override {
+        std::cout << "Крейсер: " << name << std::endl;
+        Ship::displayInfo();
+    }
+
+    void input() override {
+        std::cout << "Введіть інформацію для Крейсера:\n";
+        Ship::input();
+    }
+
+    void saveToFile(std::ofstream& outFile) override {
+        outFile << "Крейсер\n";
+        Ship::saveToFile(outFile);
+    }
+
+    void loadFromFile(std::ifstream& inFile) override {
+        std::string temp;
+        std::getline(inFile, temp); // Читання "Крейсер"
+        Ship::loadFromFile(inFile);
+    }
 };
 
 #endif // CRUISER_H
+
